93ff57a0342667612dc51119d3fcc9e73070c68f
RaymondCoder27 <mwebraymond13@gmail.com>
2024-10-30 22:34:36 +0300

    fixes backend file formatting

--- whatsappClone/src/views/FindFriendsView.vue
+++ whatsappClone/src/views/FindFriendsView.vue
@@ -1,28 +1,23 @@
 <script setup>
-
 </script>
 <template>
-  <div id="FindFriends" class="z-0 overflow-auto pt-[100px] fixed h-[100vh] w-full">
+  <div
+    id="FindFriends"
+    class="z-0 overflow-auto pt-[100px] fixed h-[100vh] w-full"
+  >
     <div class="flex w-full p-4 items-center cursor-pointer">
       <img src="../assets/images/profile.jpeg" class="rounded-full mr-4 w-12" />
 
       <div class="w-full">
-       <div class="flex justify-between items-center">
-        <div class="text-[15px] text-gray-600">Frank</div>
-      </div>
-
-        <div class="flex items-center">
-          <div class="text-[15px] text-gray-500 ">
-            Hi, I'm using Whatsapp!
-          </div>
+        <div class="flex justify-between items-center">
+          <div class="text-[15px] text-gray-600">Frank</div>
         </div>
 
+        <div class="flex items-center">
+          <div class="text-[15px] text-gray-500">Hi, I'm using Whatsapp!</div>
+        </div>
       </div>
     </div>
-
-
-
-
   </div>
 </template>
 
